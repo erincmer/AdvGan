9edacf06da317e2d46cea859b97d5aa60ab14217
erincmer <merdivane@gmail.com>
2018-01-27 11:12:35 +0100

    Add files via upload

---
+++ headerSeq2Seq.py
@@ -0,0 +1,24 @@
+# Max dialogue length
+MAX_SEQ_LENGTH = 200
+
+# Max sentence length
+REP_SEQ_LENGTH=20
+
+# Batch size
+BATCH_SIZE = 16
+
+# RNN hidden dimension
+HIDDEN_DIM = 250
+
+# Set to True to delete api_call from dataset
+DELETE_API_CALL = False
+
+START_TOKEN = 0
+
+PRE_EPOCH_NUM = 120
+
+SEED = 88
+BATCH_SIZE = 64
+MC_NUM = 5
+
+DO_RESTORE = False
